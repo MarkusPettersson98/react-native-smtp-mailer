44c817c33dc3e68c059f39cbe915304ab30f0b3a
Markus Pettersson <markus.pettersson1998@gmail.com>
2020-03-28 14:58:10 +0100

    Remove deprecated function in Android package

--- android/src/main/java/com/reactlibrary/RNSmtpMailerPackage.kt
+++ android/src/main/java/com/reactlibrary/RNSmtpMailerPackage.kt
@@ -5,9 +5,6 @@ import com.facebook.react.bridge.*
 import com.facebook.react.uimanager.ViewManager
 
 class RNSmtpMailerPackage : ReactPackage {
-    override fun createJSModules(): MutableList<Class<out JavaScriptModule>> {
-        TODO("not implemented. Deprecated since React-native 0.47")
-    }
 
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
         return listOf<NativeModule>(RNSmtpMailerModule(reactContext))
